fc0459b7ab18638f7345d15c653d2b826795fcc7
zhyang <zhyanglyuee@gmail.com>
2020-05-12 21:07:20 +0800

    first update

---
+++ controllers/my_controller/README.md
@@ -0,0 +1,5 @@
+# README
+
+在編譯之前，需要先執行以下指令
+
+    export WEBOTS_HOME=/usr/local/webots
---
+++ controllers/my_controller/my_controller.cpp
@@ -0,0 +1,60 @@
+// File:          my_controller.cpp
+// Date:
+// Description:
+// Author:
+// Modifications:
+
+// You may need to add webots include files such as
+// <webots/DistanceSensor.hpp>, <webots/Motor.hpp>, etc.
+// and/or to add some other includes
+#include <webots/Robot.hpp>
+#include <webots/Motor.hpp>
+// All the webots classes are defined in the "webots" namespace
+using namespace webots;
+
+// This is the main program of your controller.
+// It creates an instance of your Robot instance, launches its
+// function(s) and destroys it at the end of the execution.
+// Note that only one instance of Robot should be created in
+// a controller program.
+// The arguments of the main function can be specified by the
+// "controllerArgs" field of the Robot node
+int main(int argc, char **argv) {
+  // create the Robot instance.
+  Robot *robot = new Robot();
+
+  // get the time step of the current world.
+  int timeStep = (int)robot->getBasicTimeStep();
+
+  // You should insert a getDevice-like function in order to get the
+  // instance of a device of the robot. Something like:
+  //  Motor *motor = robot->getMotor("motorname");
+  //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
+  //  ds->enable(timeStep);
+  Motor *wheel1 = robot->getMotor("wheel1");
+  Motor *wheel2 = robot->getMotor("wheel2");
+  Motor *wheel3 = robot->getMotor("wheel3");
+  Motor *wheel4 = robot->getMotor("wheel4");
+
+  // Main loop:
+  // - perform simulation steps until Webots is stopping the controller
+  while (robot->step(timeStep) != -1) {
+    // Read the sensors:
+    // Enter here functions to read sensor data, like:
+    //  double val = ds->getValue();
+
+    // Process sensor data here.
+
+    // Enter here functions to send actuator commands, like:
+    //motor->setPosition(10.0);
+    wheel1->setPosition(50);
+    wheel2->setPosition(50);
+    wheel3->setPosition(50);
+    wheel4->setPosition(50);
+  };
+
+  // Enter here exit cleanup code.
+
+  delete robot;
+  return 0;
+}
